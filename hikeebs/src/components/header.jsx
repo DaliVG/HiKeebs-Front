d84dfbe4871bb4ff0734dec4e6fbe0eed4255457
DaliVidic <daliborka_vg@hotmail.com>
2023-05-16 23:23:11 +0100

    Main page DONE

--- hikeebs/src/components/header.jsx
+++ hikeebs/src/components/header.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import keycapNav from '../img/keycapnav.png';
+import brand from '../img/marca.png'
 import '../navbar.css';
 
 function Header() {
@@ -9,17 +10,25 @@ function Header() {
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href=".">
-        <img
+          <ul className="bran-icon">
+            <li>
+            <img
               src={keycapNav}
               width="120"
               height="120"
               className="d-inline-block align-top"
               alt="keycaplogo"
             />
+            </li>
+            <li>
+<img src={brand} className="App-marca" alt="marca" width="250px"
+        height="80px"/>
+            </li>
+          </ul>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="gap-4">
             <Nav.Link href=".">Home</Nav.Link>
             <Nav.Link href="/aboutUs">About us</Nav.Link>
             <Nav.Link href="/lista">Keycaps</Nav.Link>
