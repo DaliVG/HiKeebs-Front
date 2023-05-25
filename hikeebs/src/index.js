45e5f663694a7f16b691ac0517280ff824805e72
DaliVidic <daliborka_vg@hotmail.com>
2023-05-25 11:27:34 +0100

    Perdon vivi ya parod e tocar tu rama

--- hikeebs/src/index.js
+++ hikeebs/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { HashRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HashRouter base="/">
-    <App />
-  </HashRouter>);
+root.render(
+    <App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
